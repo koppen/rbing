ac11cc0f1653331f887b605830a85c69b227602b
Jakob Skjerning <jakob@mentalized.net>
2012-08-10 23:51:46 +0200

    Remove specs for unsupported sources

--- spec/rbing_spec.rb
+++ spec/rbing_spec.rb
@@ -6,34 +6,6 @@ describe "RBing" do
     @bing = RBing.new
   end
   
-  it "should provide an ad search method" do
-    @bing.should respond_to(:ad)
-  end
-  
-  it "should provide an image search method" do
-    @bing.should respond_to(:image)
-  end
-  
-  it "should provide an instant_answer search method" do
-    @bing.should respond_to(:instant_answer)
-  end
-  
-  it "should provide a news search method" do
-    @bing.should respond_to(:news)
-  end
-  
-  it "should provide a phonebook search method" do
-    @bing.should respond_to(:phonebook)
-  end
-  
-  it "should provide a related_search method" do
-    @bing.should respond_to(:related_search)
-  end
-  
-  it "should provide an spell method" do
-    @bing.should respond_to(:spell)
-  end
-  
   it "should provide a web search method" do
     @bing.should respond_to(:web)
   end

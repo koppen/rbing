b8e5cb055ddf220190cec09e4936812bb7740a0b
Jakob Skjerning <jakob@mentalized.net>
2015-04-24 09:16:03 +0200

    Clean up syntax

--- spec/rbing_spec.rb
+++ spec/rbing_spec.rb
@@ -1,24 +1,24 @@
-require File.dirname(__FILE__) + '/spec_helper'
-require 'rbing'
+require File.dirname(__FILE__) + "/spec_helper"
+require "rbing"
 
 describe "RBing" do
   before :each do
     @bing = RBing.new
   end
-  
+
   it "should format a query properly with a hash" do
-    @bing.send(:build_query, "ruby").should eq({:Query=>"'ruby'"})
+    @bing.send(:build_query, "ruby").should eq(:Query => "'ruby'")
   end
-  
+
   it "should provide a web search method" do
     @bing.should respond_to(:web)
   end
-  
+
   it "should return a ResponseData object" do
-    @bing.web("ruby", top: 10, skip: 10).should be_a(RBing::ResponseData)
+    @bing.web("ruby", :top => 10, :skip => 10).should be_a(RBing::ResponseData)
   end
-  
+
   it "should return search results" do
-    @bing.web("ruby", top: 10, skip: 10).results.should_not be_empty
+    @bing.web("ruby", :top => 10, :skip => 10).results.should_not be_empty
   end
 end
